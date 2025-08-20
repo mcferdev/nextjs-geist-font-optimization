646e773975875ce2f701410e1508ed84b71412b8
Marcelo C Fernandes <marcelocferdeveloper@gmail.com>
2025-08-20 22:05:30 +0100

    Add .next/static/chunks/src_app_properties_[id]_page_tsx_133e1f16._.js 1755723930037

---
+++ .next/static/chunks/src_app_properties_[id]_page_tsx_133e1f16._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_d70cfcf3._.js",
+  "static/chunks/node_modules_3ce0805e._.js"
+],
+    source: "dynamic"
+});
